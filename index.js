37c6347d890d1eb110975a21a01f85407a3f6300
caye <caye1300@gmail.com>
2020-10-21 22:05:17 -0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Heyy its Caye!')
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Port 3000!')
+})
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
