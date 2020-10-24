ae5a61ead7430689d431885af27b3b43e91b8ecc
caye <caye1300@gmail.com>
2020-10-24 15:37:51 -0700

    events

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const express = require('express')
 const app = express()
-// app.get('/', (req, res) => {
-//   res.send('Heyy its Caye!')
-// })
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log('Port 3000!')
-})
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+
 
 var events = [
   { title: "I am your first event", desc: "7th Annual SF Rock Project Festive Festival of Bands", imgUrl: "https://scontent-fml2-1.xx.fbcdn.net/v/t1.0-9/78280659_2969609699718440_4181947031197057024_o.jpg?_nc_cat=108&ccb=2&_nc_sid=b386c4&_nc_ohc=g0ueaEzlSPMAX8mwIWC&_nc_ht=scontent-fml2-1.xx&oh=e182f88e7ecdd1e3f00a652f1aa85659&oe=5FB874C4" },
@@ -19,4 +10,12 @@ var events = [
 
 app.get('/', (req, res) => {
   res.render('events-index', { events: events });
+})
+
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Port 3000!')
 })
