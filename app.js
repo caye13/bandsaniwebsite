3fea1c82d5d8c65081e43275ec7e0a3f012ab660
caye <caye1300@gmail.com>
2020-11-04 22:34:45 -0800

    get events for ind view

--- app.js
+++ app.js
@@ -20,17 +20,17 @@ app.get('/', (req, res) => {
 //   })
 // })
 
-app.get('/events/new', (req, res) => {
-  res.render('events-new', {});
-})
-
-// app.get('/events/:id', (req, res) => {
-//   models.Event.findByPk(req.params.id).then((event) => {
-//     res.render('events-show', { event: event })
-//   }).catch((err) => {
-//     console.log(err.message);
-//   })
+// app.get('/events/new', (req, res) => {
+//   res.render('events-new', {});
 // })
+
+app.get('/events/:id', (req, res) => {
+  models.Event.findByPk(req.params.id).then((event) => {
+    res.render('events-show', { event: event })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 app.get('/events/:id', (req, res) => {
   res.send('I\'m an event')
